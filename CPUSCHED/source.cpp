9ed5bd009f2703777af6307a5e770a32c79393dd
Ethan Landers <eland007@odu.edu>
2023-10-03 17:29:40 -0400

    updating source.cpp

--- CPUSCHED/source.cpp
+++ CPUSCHED/source.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-class CPUprocess{
-    int arrivalTime;
-    int CPU_BurstLength;
-    int priority;
+class CPU_Process{
+    public:
+        int arrivalTime;
+        int CPU_BurstLength;
+        int priority;
+
+    CPU_Process(int arrivalTime, int CPU_BurstLength, int priority){
+        arrivalTime = 0;
+        CPU_BurstLength = 0;
+        priority = 0;
+    }
+
+
 };
 
 int main(){
@@ -20,7 +30,11 @@ void readData(){
     ifstream inFile;
     inFile.open("datafile.txt");
 
+    CPU_Process process[];
+    
     if(inFile.is_open()){
+        // for(i=0; i<)     LOOP THROUGH FILE
+        inFile >> process->arrivalTime >> process->CPU_BurstLength >> process->priority;
         inFile.close();
     }
     else{
