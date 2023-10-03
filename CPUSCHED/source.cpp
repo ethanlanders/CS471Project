af4003dd6003268ca590eb3c72637c2a5def6941
Ethan Landers <eland007@odu.edu>
2023-10-03 17:14:29 -0400

    created readData(), creating CPUprocess class

--- CPUSCHED/source.cpp
+++ CPUSCHED/source.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
+class CPUprocess{
+    int arrivalTime;
+    int CPU_BurstLength;
+    int priority;
+};
+
 int main(){
-    cout << "Hello World!\n";
+    readData();
     return 0;
 }
 
-//Testing this.  No
+void readData(){
+    //string inFileName = "datafile.txt"
+    ifstream inFile;
+    inFile.open("datafile.txt");
+
+    if(inFile.is_open()){
+        inFile.close();
+    }
+    else{
+        cerr << "Cannot find that input file." << endl;
+    }
+}
+
+void FIFO(){
+
+
+}
+
+void SJF(){
+
+
+}
+
+void preemptivePriority(){
+    
+}
