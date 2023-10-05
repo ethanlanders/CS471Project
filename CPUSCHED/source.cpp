a7e3b73a183f73f2f92a5124fd50ac347f9d8239
Ethan Landers <eland007@odu.edu>
2023-10-05 17:00:47 -0400

    success with read in from .txt

--- CPUSCHED/source.cpp
+++ CPUSCHED/source.cpp
@@ -16,17 +16,26 @@ struct CPU_Process{
 void readData(){
     //string inFileName = "datafile.txt"
     ifstream inFile;
+    string firstLine;
     inFile.open("datafile.txt");
 
     CPU_Process process[numOfProcesses];
     
     if(inFile.is_open()){
-        // for(i=0; i<)     LOOP THROUGH FILE
+        getline(inFile, firstLine);
+        cout << firstLine;
+        for(int i=0; i<numOfProcesses; i++){
+            inFile >> process[i].arrivalTime >> process[i].CPU_BurstLength >> process[i].priority;
+        }
         inFile.close();
     }
     else{
         cerr << "Cannot find that input file." << endl;
     }
+    // // Testing output
+    // for(int i=0; i<numOfProcesses; i++){
+    //     cout << process[i].arrivalTime << "   " << process[i].CPU_BurstLength << "   " << process[i].priority << endl;
+    // }
 }
 
 void FIFO(){
