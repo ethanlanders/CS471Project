f71f48714795637673281637ae6f40557bf77f7d
Charles Milroy <cmilr001@odu.edu>
2023-10-31 15:57:45 -0400

    Fixing variables and timing

--- PRODUCER-CONSUMER/source.cpp
+++ PRODUCER-CONSUMER/source.cpp
@@ -13,8 +13,9 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 using namespace std;
-bool ready = false; // Tell threads to run
-std::mutex buf;     // mutex for critical section
+bool ready = true; // Tell threads to run
+bool quit = false; // End threads
+std::mutex buf;    // mutex for critical section
 condition_variable cv;
 
 // Per textbook problem statement
@@ -24,35 +25,37 @@ vector<int> buffer;
 #define BUFFER_SIZE 5
 
 // buffer_item buffer[BUFFER_SIZE];
-  
+
 // buffer Functions
 bool insert_item(int item)
 {
     bool insert = false;
     unique_lock<mutex> lock(buf);
 
-    if(buffer.size() < BUFFER_SIZE){
+    if (buffer.size() < BUFFER_SIZE)
+    {
         buffer.push_back(item);
-        if(buffer.back() == item)
+        if (buffer.back() == item)
             insert = true;
     }
 
-    cv.notify_one();
+    cv.notify_all();
     return insert;
-} 
+}
 
-bool remove_item(int& item)
+bool remove_item(int &item)
 {
     bool remove = false;
     unique_lock<mutex> lock(buf);
 
-    if(buffer.size() > 0){
+    if (buffer.size() > 0)
+    {
         item = buffer.front();
         buffer.erase(buffer.begin());
         remove = true;
     }
- 
-    cv.notify_one();
+
+    cv.notify_all();
     return remove;
 }
 
@@ -61,14 +64,16 @@ void producer()
 {
     // buffer_item item;
     int item = 0;
-    sleep(1);
 
-    while (ready)
+    while (!quit)
     {
+        while (ready)
+        {
+        }
         /* sleep for a random period of time from 0-5 seconds */
         sleep(rand() % 5);
         /* generate a random number */
-        item = rand()%100;
+        item = rand() % 100;
         if (!insert_item(item)) // insert_item(item))
             cout << "Producer Error: Full buffer\n";
         else
@@ -81,10 +86,12 @@ void consumer()
 {
     // buffer_item item;
     int item = 0;
-    sleep(1);
 
-    while (ready)
+    while (!quit)
     {
+        while (ready)
+        {
+        }
         /* sleep for a random period of time from 0-5 seconds */
         sleep(rand() % 5);
         if (!remove_item(item)) // remove_item(&item))
@@ -98,16 +105,16 @@ void consumer()
 /* Changes ready to true, and begins the threads printing */
 void runThreads(int time)
 {
-    ready = true;
-    sleep(time);
     ready = false;
+    sleep(time);
+    quit = true;
 }
 
 int main(int argc, char *argv[])
 {
     // Get CLI input
     int time = 20;
-    int producerNum = 1;
+    int producerNum = 16;
     int consumerNum = 1;
 
     // Initialize Buffer
@@ -117,14 +124,11 @@ int main(int argc, char *argv[])
     std::thread cons[consumerNum];
     std::thread timer;
 
-    /* spawn producer threads */
-    timer = std::thread(runThreads, time);
-
     for (int id = 0; id < producerNum; id++)
         prod[id] = std::thread(producer);
     cout << "Created Producer Threads\n";
     /* spawn consumer threads */
-    for (int id = 0; id < producerNum; id++)
+    for (int id = 0; id < consumerNum; id++)
         cons[id] = std::thread(consumer);
     cout << "Created Consumer Threads\n";
 
@@ -132,13 +136,16 @@ int main(int argc, char *argv[])
     std::cout << " in parallel: \n"
               << std::endl;
 
+    /* Starts Threads */
+    timer = std::thread(runThreads, time);
 
     /* close producer threads */
     for (int id = 0; id < producerNum; id++)
         prod[id].join();
     /* close consumer threads */
-    for (int id = 0; id < producerNum; id++)
+    for (int id = 0; id < consumerNum; id++)
         cons[id].join();
+    /* Close Timer Thread */
     timer.join();
 
     cout << "\n";
